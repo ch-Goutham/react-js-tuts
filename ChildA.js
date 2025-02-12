033a2637205f8d98be014939ebf070a65bb0a5be
ch-Goutham <gouthamyadav202@gmail.com>
2025-02-12 10:26:24 +0530

    Add files via upload

---
+++ ChildA.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const ChildA=()=>{
+    console.log("Child A rendered")
+    return <h2>This is Child A</h2>
+};
+export default ChildA;
---
+++ ChildB.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import { memo } from 'react'
+
+const ChildB = ({count, increment}) => {
+    console.log("Child B rendered")
+
+  return (
+    <div>
+        <h1>Count:{count}</h1>
+        <button onClick={increment}>Increment</button>
+    </div>
+  )
+}
+
+export default memo(ChildB);
+
