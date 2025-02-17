e1826b98844e857cb1fb89cfd9d91bfe069db381
ch-Goutham <gouthamyadav202@gmail.com>
2025-02-17 10:36:37 +0530

    Add files via upload

--- App.js
+++ App.js
@@ -98,27 +98,157 @@
 
 // export default AppleCounter;
 
-import React,{useState,useEffect,useRef}from 'react';
+// import React,{useState,useEffect,useRef}from 'react';
 
-function CounterWithPrev(){
-  const[count,setCount]=useState(0);
-  const preCountRef=useRef();
-  useEffect(()=>{
-    preCountRef.current=count;
+// function CounterWithPrev(){
+//   const[count,setCount]=useState(0);
+//   const preCountRef=useRef();
+//   useEffect(()=>{
+//     preCountRef.current=count;
 
-  },[count])
-  return(
-    <div>
-      <p>current count:{count}</p>
+//   },[count])
+//   return(
+//     <div>
+//       <p>current count:{count}</p>
     
-     <p>  prevCount : {preCountRef.current}</p>
-     <button onClick={()=> setCount(count+1)}>Increase Count</button>
+//      <p>  prevCount : {preCountRef.current}</p>
+//      <button onClick={()=> setCount(count+1)}>Increase Count</button>
         
       
-    </div>
+//     </div>
   
       
   
+//   )
+// }
+// export default CounterWithPrev;
+// import React, { useReducer } from 'react';
+// import './App.css';
+// import confetti from 'canvas-confetti';
+
+// function appleReducer(state, action) {
+//   switch (action.type) {
+//     case 'ADD_APPLE':
+//       return state + 1;
+//     case 'EAT_APPLE':
+//       return state > 0 ? state - 1 : state;
+//     default:
+//       return state;
+//   }
+// }
+
+// function App() {
+//   const [state, dispatch] = useReducer(appleReducer, 4);
+
+//   const handleEatApple = () => {
+//     if (state > 0) {
+//       dispatch({ type: 'EAT_APPLE' });
+//       confetti({
+//         particleCount: 100,
+//         spread: 70,
+//         origin: { y: 0.6 }
+//       });
+//     }
+//   };
+//   const handleAddApple = () => {
+//     if (state > 0) {
+//       confetti({
+//         particleCount: 200,
+//         spread: 70,
+//         origin: { y: 0.6 }
+//       });
+//     }
+//   };
+
+//   return (
+//     <div>
+//       <center>
+//       <h1>NUMBER OF APPLES: {state}</h1>
+//       <div style={{ display: 'flex', flexDirection: 'row', flexWrap: 'wrap' }}>
+//         {Array.from({ length: state }).map((_, index) => (
+//           <img
+//             key={index}
+//             src="https://thumbs.dreamstime.com/b/red-apple-isolated-clipping-path-19130134.jpg"
+//             alt="Apple"
+//             style={{ width: '150px', height: '200px', margin: '5px' }}
+//           />
+//         ))}
+//       </div>
+//       <a onClick={handleAddApple}>
+//       <button className='a' onClick={() => dispatch({ type: 'ADD_APPLE' }) } >ADD APPLE</button>
+//      </a>
+//       {/* onClick={() => dispatch({ type: 'ADD_APPLE' }) }> */}
+        
+//       <button className='b' onClick={handleEatApple}>EAT APPLE</button>
+//       </center>
+//     </div>
+//   );
+// }
+
+// export default App;
+
+// Event Handling
+// 1.onClick
+// Ex:
+// function App(){
+//   const handleClick =()=>{
+//     alert("Button Clicked");
+//   }
+//   return(
+//     <button onClick={handleClick}>Click Me!</button>
+//   )
+// }
+// export default App;
+
+// 2.Change event(onChange)
+// This happens when you type in a text box
+// Key Points
+// Target is aproperty of the event object, and is predefined by javascript fro DOM events
+// event.target : always refers the element that triggered the event 
+// event.target.value : The value of input elements buttons, form elements
+
+// Ex:
+// import React, {useState} from "react";
+// function App(){
+//   const[text, setText] = useState("");
+//   // initially the value is set as empty string
+//    const handleChange=(event)=>{
+//     setText(event.target.value)
+//     // update the text state with the value input 
+//   }
+//   return(
+//     <div>
+//       <input type="text" value={text} onChange={handleChange} />
+//       <p>Your Text: {text} </p>
+//     </div>
+//   )
+// }
+
+// export default App;
+
+// Mouse events(onMouseEnter, onMouseLeave)
+// Thsis happens when we move the mouse over or away from the elements 
+
+// Ex:
+import React, {useState} from "react";
+function App(){
+  const[isHovered, setIsHovered] = useState(false);
+
+  const handleMouseEnter=()=>{
+    setIsHovered(true);
+    // set as true when mouse is over
+  }
+  const handleMouseLeave = ()=>{
+    setIsHovered(false);
+    // set as false when mouse is leaves
+  }
+  return(
+    <div>
+      <button onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}
+      style={{backgroundColor: isHovered ? 'lightblue' : 'lightgray', color: isHovered? 'red' : 'blue'}}
+      >Hover Me!</button>
+      {isHovered && <p>Mouse is over the button</p>}
+    </div>
   )
 }
-export default CounterWithPrev;
+export default App;
